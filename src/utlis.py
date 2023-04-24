1d089ec3ae600e6f0eead1017213a208c2cc5430
Prashant-Tomar <thepiratekingpersie@gmail.com>
2023-04-24 19:23:51 +0530

    Deployment config

--- src/utlis.py
+++ src/utlis.py
@@ -8,6 +8,7 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -24,14 +25,20 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
-            model.fit(X_train, y_train) #Train Model
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
@@ -46,3 +53,11 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
 
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
